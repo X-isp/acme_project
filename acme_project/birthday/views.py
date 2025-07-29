5c6f623e65622631272b32153da8a86d1e3cec07
Anastasia Stukalova <a.d.stukalova@yandex.ru>
2025-07-29 13:49:06 +0300

    Get-posts

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,6 +1,49 @@
+# birthday/views.py 
 from django.shortcuts import render
+
+from .forms import BirthdayForm
+# Импортируем из utils.py функцию для подсчёта дней.
+from .utils import calculate_birthday_countdown
+
+
+# def birthday(request):
+#     print(request.GET)
+#     form = BirthdayForm()
+#     # Добавляем его в словарь контекста под ключом form:
+#     context = {'form': form}
+#     # Указываем нужный шаблон и передаём в него словарь контекста.
+#     return render(request, 'birthday/birthday.html', context)
+
+# def birthday(request):
+#     # Если есть параметры GET-запроса...
+#     if request.GET:
+#         # ...передаём параметры запроса в конструктор класса формы.
+#         form = BirthdayForm(request.GET)
+#         # Если данные валидны...
+#         if form.is_valid():
+#             # ...то считаем, сколько дней осталось до дня рождения.
+#             # Пока функции для подсчёта дней нет — поставим pass:
+#             pass
+#     # Если нет параметров GET-запроса.
+#     else:
+#         # То просто создаём пустую форму.
+#         form = BirthdayForm()
+#     # Передаём форму в словарь контекста:
+#     context = {'form': form}
+#     return render(request, 'birthday/birthday.html', context)
 
 
 def birthday(request):
-    context = {}
-    return render(request, 'birthday/birthday.html', context=context)
+    form = BirthdayForm(request.GET or None)
+    # Создаём словарь контекста сразу после инициализации формы.
+    context = {'form': form}
+    # Если форма валидна...
+    if form.is_valid():
+        # ...вызовем функцию подсчёта дней:
+        birthday_countdown = calculate_birthday_countdown(
+            # ...и передаём в неё дату из словаря cleaned_data.
+            form.cleaned_data['birthday']
+        )
+        # Обновляем словарь контекста: добавляем в него новый элемент.
+        context.update({'birthday_countdown': birthday_countdown})
+    return render(request, 'birthday/birthday.html', context)
